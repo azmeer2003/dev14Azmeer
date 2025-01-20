095732efd6ae07f768418e0801cd3d7199b7c7b6
Muhammad Azmeer <l1f21bsse0615@ucp.edu.pk>
2025-01-20 09:03:28 +0500

    Initial commit

---
+++ configurations.sh
@@ -0,0 +1,8 @@
+echo "Repo Initial Configuration Started"
+git branch -m develop
+git config --local user.name "Muhammad Azmeer"
+git config --local user.email l1f21bsse0615@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./style
+touch ./src/index.html ./style/index.css .gitignore
+npm init -y
